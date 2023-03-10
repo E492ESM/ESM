7b2efb0edb436b5fa9f8effe622f3b8de646cdb0
LeoLrw <leolrw1@gmail.com>
2023-03-10 14:58:59 -0700

    Moved code into main func

--- Mic.py
+++ Mic.py
@@ -7,23 +7,6 @@ from datetime import datetime
 import gdown
 import RPi.GPIO as GPIO
 
-#Mic Configuration
-chunk = 9600  # Record in chunks of 9600 samples
-sampleFormat = pyaudio.paInt16  # 16 bits per sample
-channels = 1
-samplingRate = 384000  # Record at 384000 samples per second
-
-#default settings, should be overwritten by the config update
-moduleName = "ESM"
-contLength = 30
-trigLengthBefore = 15
-trigLengthAfter = 15
-
-#pin settings
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
 def updateConfig():
     #Download the config file from Google Drive
     url = 'https://drive.google.com/uc?id=1JLCfnInWqMLgsnM06CptSa6VYjuuY1tZ'
@@ -43,8 +26,10 @@ def updateConfig():
     trigLengthBefore = int(trigLengthBefore.split(":")[1].lstrip())
     trigLengthAfter = int(trigLengthAfter.split(":")[1].lstrip())
 
+    return moduleName, contLength, trigLengthBefore, trigLengthAfter
+
 #Continuous Mode
-def startContRecording():
+def startContRecording(contLength):
     #Based on code from https://realpython.com/playing-and-recording-sound-python/
     continuousModeLength = contLength*60 # Length of recording in seconds
 
@@ -76,40 +61,59 @@ def startContRecording():
     wf.writeframes(b''.join(frames))
     wf.close()
 
-updateConfig()
-p = pyaudio.PyAudio()  # Create an interface to PortAudio
-stream = p.open(format=sampleFormat, channels=channels, rate=samplingRate, frames_per_buffer=chunk, input=True)
-audioQueue = deque(maxlen=int((trigLengthBefore+trigLengthAfter)*samplingRate/chunk))
+if __name__ == "__main__":
+    #Mic Configuration
+    chunk = 9600  # Record in chunks of 9600 samples
+    sampleFormat = pyaudio.paInt16  # 16 bits per sample
+    channels = 1
+    samplingRate = 384000  # Record at 384000 samples per second
 
-while True:
-    audioQueue.append(stream.read(chunk))
-    #Continuous Mode
-    if GPIO.input(10) == GPIO.HIGH:
-        updateConfig()
-        startTime = datetime.now()
-        thread = Thread(target=startContRecording)
-        thread.start()
-    #Triggered Mode    
-    if GPIO.input(11) == GPIO.HIGH:
-        savedAudioQueue = audioQueue
-        frames = []
-        for elem in savedAudioQueue:
-            frames.append(elem)
-        filename = moduleName + " - " + startTime.strftime("%d-%m-%Y %H-%M-%S") + ".wav"
+    #default settings, should be overwritten by the config update
+    moduleName = "ESM"
+    contLength = 30
+    trigLengthBefore = 15
+    trigLengthAfter = 15
 
-        # Save the recorded data as a WAV file
-        wf = wave.open(filename, 'wb')
-        wf.setnchannels(channels)
-        wf.setsampwidth(p.get_sample_size(sampleFormat))
-        wf.setframerate(samplingRate)
-        wf.writeframes(b''.join(frames))
-        wf.close()
+    #pin settings
+    GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+    GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# Stop and close the stream 
-stream.stop_stream()
-stream.close()
-# Terminate the PortAudio interface
-p.terminate()
+    moduleName, contLength, trigLengthBefore, trigLengthAfter = updateConfig()
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+    stream = p.open(format=sampleFormat, channels=channels, rate=samplingRate, frames_per_buffer=chunk, input=True)
+    audioQueue = deque(maxlen=int((trigLengthBefore+trigLengthAfter)*samplingRate/chunk))
+
+    while True:
+        audioQueue.append(stream.read(chunk))
+        #Continuous Mode
+        if GPIO.input(10) == GPIO.HIGH:
+            moduleName, contLength, trigLengthBefore, trigLengthAfter = updateConfig()
+            startTime = datetime.now()
+            thread = Thread(target=startContRecording, args=(contLength,))
+            thread.start()
+        #Triggered Mode    
+        if GPIO.input(11) == GPIO.HIGH:
+            savedAudioQueue = audioQueue
+            startTime = datetime.now()
+            frames = []
+            for elem in savedAudioQueue:
+                frames.append(elem)
+            filename = moduleName + " - " + startTime.strftime("%d-%m-%Y %H-%M-%S") + ".wav"
+
+            # Save the recorded data as a WAV file
+            wf = wave.open(filename, 'wb')
+            wf.setnchannels(channels)
+            wf.setsampwidth(p.get_sample_size(sampleFormat))
+            wf.setframerate(samplingRate)
+            wf.writeframes(b''.join(frames))
+            wf.close()
+
+    # Stop and close the stream 
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
 
 
 
